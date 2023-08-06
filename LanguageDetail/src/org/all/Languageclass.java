456e5e081967760f1140006cae482b61747e4a48
GOKULKARTHI <gokulkarthickmech1994@gmail.com>
2023-08-06 23:17:40 +0530

    Pain destoryed the village

---
+++ LanguageDetail/src/org/all/Languageclass.java
@@ -0,0 +1,23 @@
+package org.all;
+
+import org.telgu.Telgu;
+
+public class Languageclass extends Telgu {
+
+	public void alllanguage() {
+	
+	System.out.println("All Language Course Fee");
+	
+	}
+	public static void main(String[] args) {
+
+		Languageclass all = new Languageclass();
+		
+		all.alllanguage();
+		all.tamillanguage();
+		all.telgulanguage();
+		all.englishlanguage();
+	
+	}
+
+}
